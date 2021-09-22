5cf73b6157ea3f3759dda43aca36773b7745dd9e
Xan.olazcuagagaribal <xan.olazcuagagaribal@gmail.com>
2021-09-22 15:01:58 +0200

    Basic test for 0

--- src/ExoTDD.php
+++ src/ExoTDD.php
@@ -4,5 +4,17 @@ use phpDocumentor\Reflection\Types\Boolean;
 
 class ExoTDD
 {
-    
+    /*
+    1 - En utilisant la méthode TDD et dans le langage de votre choix, créer une fonction decimalToRoman qui prend en paramètre un entier 
+    entre 0 et 3 000 et renvoie sa représentation en chiffres romains sous forme de chaîne de caractères.
+    Vous pouvez commencer par des tests simples : 0 (“”), 1 (“I”), 2 (“II”), 4 (“IV”), 5 (“V”), 6 (“VI”)… puis des nombres à plusieurs chiffres.
+    */
+    public static function decimalToRoman($number){
+        return "";
+    }
+    /*
+    2 - En utilisant la méthode TDD et dans le langage de votre choix, créer une fonction romanToDecimal qui prend en 
+    paramètre une chaîne de caractères représentant un entier entre 0 et 3 000 et renvoie cet entier. (6 points)
+
+    */
 }
--- tests/exoTddTest.php
+++ tests/exoTddTest.php
@@ -4,6 +4,9 @@ use PHPUnit\Framework\TestCase;
 
 final class ExoTddTest extends TestCase
 {
-    
+    public function testDecimalToRoman(): void
+    {
+        $this->assertEquals("", ExoTDD::decimalToRoman(0));   
+    }
     
 }
